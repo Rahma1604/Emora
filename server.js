a627bd1abffe404ac86d6f5466918b02e81168e7
Rahma1604 <rahmamostafa041@gmail.com>
2026-03-08 21:25:37 +0200

    Successfully linked MongoDB and added Cloudinary config

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+const app = express();
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected Successfully"))
+    .catch(err => console.error("Database Connection Error", err));
+
 app.get('/', (req, res) => {
-    res.send("Backend is Running!");
+    res.send("Emora Backend is running!");
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});v
+    console.log(`Server is moving on port ${PORT} 🚀`);
+});
