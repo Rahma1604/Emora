3d9fffcd6caad1ae5be543acd3afc6d10b24310e
RadwaOmar23 <ro9582198@gmail.com>
2026-06-21 19:13:38 +0300

    remove challenge

--- AI/challenge_engine/game_pool.py
+++
@@ -1,7 +0,0 @@
-GAME_POOL = {
-    "sad": ["draw_happy_place", "gratitude_list"],
-    "angry": ["breathing_game", "stress_pop"],
-    "anxious": ["safe_place_draw", "calm_audio"],
-    "happy": ["puzzle_game"],
-    "neutral": ["mood_picker"]
-}
