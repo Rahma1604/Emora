081c7b8c9b7a26179cdac725021ef3ed9ec3b8f2
Shahd Hesham <shoudyhesham2003@gmail.com>
2026-06-23 20:03:16 +0300

    Improve token authentication validation

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,25 +2,23 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const checkToken = async(req, res, next) => {
-    let token=req.header('x-auth-token');
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-      token=req.headers.authorization.split(' ')[1];
-    }
+  let token=req.header('x-auth-token');
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    token=req.headers.authorization.split(' ')[1];
+  }
   if (!token) {
-        return res.status(401).json({ message: 'Please login first' });
-    }
+    return res.status(401).json({ message: 'Please login first' });
+  }
   
-      try {
-    
+  try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id).select('-password');
-    next();
+    if (!req.user) {
+      return res.status(401).json({ message: 'User account not found' });
+      }
+    return next();
   } catch (error) {
-    res.status(400).json({ message: 'Invalid token.' });
-  
-};
-if (!token) {
-  res.status(401).json({ message: 'Please login first' });
-}
+    return res.status(401).json({ message: 'Invalid or expired token.' });
+  }
 };
 module.exports = { checkToken };
